5874bdb6162be09c8d3c0b7881a5737f89312957
Jens Gohl <jens.gohl@otto.de>
2021-03-18 11:46:11 +0100

    update

---
+++ assets/js/data/cache-list.js
@@ -0,0 +1,59 @@
+const include = [
+
+  /*--- CSS ---*/
+  '/kappasector/assets/css/style.css',
+
+  /*--- Javascripts ---*/
+  '/kappasector/assets/js/dist/home.min.js',
+  '/kappasector/assets/js/dist/page.min.js',
+  '/kappasector/assets/js/dist/post.min.js',
+  '/kappasector/assets/js/dist/categories.min.js',
+
+  /*--- HTML ---*/
+
+  /* Tabs */
+  
+    '/tabs/categories/',
+  
+    '/tabs/tags/',
+  
+    '/tabs/archives/',
+  
+    '/tabs/map/',
+  
+    '/tabs/about/',
+  
+
+  /*--- Icons ---*/'/kappasector/assets/img/favicons/favicon.ico',
+  '/kappasector/assets/img/favicons/apple-icon.png',
+  '/kappasector/assets/img/favicons/apple-icon-precomposed.png',
+  '/kappasector/assets/img/favicons/apple-icon-57x57.png',
+  '/kappasector/assets/img/favicons/apple-icon-60x60.png',
+  '/kappasector/assets/img/favicons/apple-icon-72x72.png',
+  '/kappasector/assets/img/favicons/apple-icon-76x76.png',
+  '/kappasector/assets/img/favicons/apple-icon-114x114.png',
+  '/kappasector/assets/img/favicons/apple-icon-120x120.png',
+  '/kappasector/assets/img/favicons/apple-icon-144x144.png',
+  '/kappasector/assets/img/favicons/apple-icon-152x152.png',
+  '/kappasector/assets/img/favicons/apple-icon-180x180.png',
+  '/kappasector/assets/img/favicons/android-icon-192x192.png',
+  '/kappasector/assets/img/favicons/favicon-32x32.png',
+  '/kappasector/assets/img/favicons/favicon-96x96.png',
+  '/kappasector/assets/img/favicons/favicon-16x16.png',
+  '/kappasector/assets/img/favicons/ms-icon-144x144.png',
+  '/kappasector/assets/img/favicons/manifest.json',
+  '/kappasector/assets/img/favicons/browserconfig.xml',
+
+  /*--- Others ---*/
+
+  '/kappasector/assets/js/data/search.json',
+  '/kappasector/404.html',
+
+  '/kappasector/app.js',
+  '/kappasector/sw.js'
+];
+
+const exclude = ['/assets/js/data/pageviews.json',
+  '/img.shields.io/'
+];
+
